e55e0414e64a64428ba6978a48d4dc072aa0b4c0
niemandkun <niemandkun@yandex.ru>
2015-08-31 23:17:59 +0500

    'Indexable' was renamed to 'Indexed'

--- cmapping/base.py
+++ cmapping/base.py
@@ -3,11 +3,11 @@ import itertools
 
 class Indexed:
     ''' Counts instances of this class and gives them unique _index. '''
-    _counter = itertools.count()
+    __counter = itertools.count()
 
     def __new__(cls, *args, **kwargs):
-        obj = super(Indexable, cls).__new__(cls)
-        obj._index = next(Indexable._counter)
+        obj = super(Indexed, cls).__new__(cls)
+        obj._index = next(Indexed.__counter)
         return obj
 
 
